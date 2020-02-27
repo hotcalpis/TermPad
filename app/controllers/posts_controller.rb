fe4849b7329a1e93659295ad5670315dbef53647
hotcalpis <hokkaru3@gmail.com>
2020-02-27 21:57:05 +0900

    close #8

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,49 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
-  def new; end
+  before_action :set_post, only: %i[show edit update destroy]
+  before_action :authenticate_user!, only: %i[new create edit update destroy]
+  before_action :correct_user, only: %i[edit update destroy]
 
-  def create; end
+  def new
+    @post = Post.new
+  end
 
-  def index; end
+  def create
+    @post = current_user.posts.build(post_params)
+    @post.save ? (redirect_to @post) : (render :new)
+  end
+
+  def index
+    @posts = Post.all
+  end
 
   def show; end
 
   def edit; end
 
-  def update; end
+  def update
+    @post.update(post_params) ? (redirect_to @post) : (render :edit)
+  end
 
-  def delete; end
+  def destroy
+    @post.destroy
+    flash[:success] = '投稿を削除しました'
+    redirect_to current_user
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:theme, :description)
+  end
+
+  def correct_user
+    @post = current_user.posts.find_by(id: params[:id])
+    redirect_to root_url if @post.nil?
+  end
 end
