774c74c3935387d1d5ae0d84bb1dd192d0eee415
hotcalpis <hokkaru3@gmail.com>
2020-05-04 00:10:08 +0900

    annotate & rubocop

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,13 @@ User.create!(
   email: 'testuser@testuser.testuser',
   # password: Rails.application.credentials[:testuser_password].to_s,
   password: 'password',
-  profile: 'テストユーザーです。ユーザー編集、アカウント削除は出来ません。',
+  profile: 'テストユーザーです。ユーザー編集、アカウント削除は出来ません。'
 )
 
 100.times do |n|
   Post.create!(
     user_id: 1,
     theme: "#{n} #{Faker::Book.title}",
-    description: "本文",
+    description: '本文'
   )
 end
